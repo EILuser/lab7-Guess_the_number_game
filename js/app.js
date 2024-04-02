503284977e09d2fc0cfb0559d25aae38819a5237
EILuser <you@example.com~>
2024-04-02 21:21:49 +0500

    final version

--- js/app.js
+++ js/app.js
@@ -1,44 +1,84 @@
+// Необходимые для реализации игры функции
 function getRandomNumber(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
+function isValid(min, max, num) {
+    if (num === "") {
+        alert("Введите число!!!");
+        return false;
+    }
+    else if (isNaN(parseInt(num)) || num < min || num > max) {
+        alert(`Некорректный ввод. По правилам игры вы должны ввести число от ${min} до ${max}. Попробуйте снова`);
+        return false;
+    }
+
+    return true;
+}
+
+function isExit(num) {
+    if (num === null && confirm("Хотите ли вы закончить игру?")) {
+        return true;
+    }
+    num = "cancel";
+    return false;
+}
+
 
 function guessTheNumberGame() {
 
-    const maxNumber = 10;
+    const maxNumber = parseInt(document.getElementById("input_range").value);
     const minNumber = 0;
     const randomNumber = getRandomNumber(minNumber, maxNumber);
     const userNumberOnScreen = document.getElementById("number");
     const resultMarker = document.getElementById("marker");
     let attemptsNumber = 5;
-    let userNumber;
 
     while (true) {
         userNumber = prompt(`Введите число от ${minNumber} до ${maxNumber}`);
 
-        if (userNumber == randomNumber) {
-            confirm('Вы выиграли!!! Если хотите сыграть еще, нажмите на кнопку "Начать игру"');
-            resultMarker.src = "images/check_mark.png";
-            break;
+        if (isExit(userNumber)) {
+            return;
         }
-        else if (userNumber > randomNumber) {
-            alert("Неверно!!! Ваше число больше загаданного");
-            attemptsNumber--;
-        }
-        else if (userNumber < randomNumber) {
-            alert("Неверно!!! Ваше число меньше загаданного");
-            attemptsNumber--;
+        else if (userNumber != null) {
+            if (!isValid(minNumber, maxNumber, userNumber)) {
+                continue;
+            }
+
+            if (userNumber == randomNumber) {
+                confirm('Вы выиграли!!! Если хотите сыграть еще, нажмите на кнопку "Начать игру"');
+                resultMarker.src = "images/check_mark.png";
+                userNumberOnScreen.textContent = `${randomNumber}`;
+                return;
+            }
+            else if (userNumber > randomNumber) {
+                alert("Неверно!!! Ваше число БОЛЬШЕ загаданного");
+                attemptsNumber--;
+            }
+            else if (userNumber < randomNumber) {
+                alert("Неверно!!! Ваше число МЕНЬШЕ загаданного");
+                attemptsNumber--;
+            }
+
+
+            if (attemptsNumber === 0) {
+                confirm('Количество попыток исчерпано. Если хотите попробовать угадать число еще раз, нажмите на конпку "Начать игру"');
+                resultMarker.src = "images/cross.png";
+                userNumberOnScreen.textContent = `${randomNumber}`;
+                return;
+            }
         }
 
-        if (attemptsNumber == 0) {
-            confirm('Количество попыток исчерпано. Если хотите попробовать угадать число еще раз, нажмите на конпку "Начать игру"');
-            resultMarker.src = "images/cross.png";
-            break;
-        }
-
-        userNumberOnScreen.textContent = `${randomNumber}`;
     }
 
 }
+
+// Отображение изменения значения ползунком
+const maxNumber = document.getElementById("max_number");
+const input = document.getElementById("input_range");
+maxNumber.textContent = `Значение: ${input.value}`;
+input.addEventListener("input", event => {
+    maxNumber.textContent = `Значение: ${event.target.value}`;
+});
